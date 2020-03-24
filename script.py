6a1a157d5b1f8c950d07fb72e17627ce4be3373c
Rodolfo Carvalho <rodolfo.ueg@gmail.com>
2020-03-24 11:27:31 -0400

    First commit

---
+++ script.py
@@ -0,0 +1,101 @@
+import logging
+import os
+import os.path as path
+import re
+import shutil
+import sys
+import urllib.error
+import urllib.request
+from datetime import date, timedelta
+from io import StringIO
+from logging import StreamHandler
+
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LTChar, LTTextBox, LTTextLine
+from pdfminer.pdfdevice import PDFDevice
+from pdfminer.pdfdocument import PDFDocument, PDFTextExtractionNotAllowed
+from pdfminer.pdfinterp import PDFPageInterpreter, PDFResourceManager
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+
+handler = StreamHandler(stream=sys.stderr)
+logging.basicConfig(handlers=[handler], level=logging.DEBUG)
+
+cases_pattern = re.compile(
+    r"Cases Reported\s+=\s+(?P<cases>\d+)(.*Deaths.+?Attributed to COVID-19\s+(?P<deaths>\d+))?", re.MULTILINE)
+
+
+def download_files():
+    start_date = date(2020, 3, 20)
+    end_date = date(2020, 3, 23)
+
+    download_url = "https://www.mass.gov/doc/covid-19-cases-in-massachusetts-as-of-{}-{}/download"
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
+
+    for i in range((end_date - start_date).days + 1):
+        current = start_date + timedelta(days=i)
+
+        url = download_url.format(
+            current.strftime("%B").lower(),
+            current.strftime("%d-%Y")
+        )
+
+        output = path.join("downloads", f"{current.strftime('%Y-%m-%d')}.pdf")
+
+        if path.exists(output):
+            logging.info(f"File {output} already exists")
+            continue
+
+        print(url)
+
+        try:
+            headers = {
+                "User-agent": user_agent
+            }
+            req = urllib.request.Request(url=url, headers=headers)
+            with urllib.request.urlopen(req) as response, open(output, 'wb') as w:
+                shutil.copyfileobj(response, w)
+                logging.info(f"Downloaded file {output}")
+        except urllib.error.HTTPError as e:
+            print(type(e), e.read())
+            raise e
+
+
+def run_analisys():
+    for f in os.listdir("downloads"):
+        try:
+            file_path = path.join("downloads", f)
+            processing_date = f.replace(".pdf", "")
+            with open(file_path, 'rb') as fp:
+                logging.info(f"processing file={file_path}")
+                stats = process_document(fp)
+            print(f"{processing_date},{stats['cases']},{stats['deaths']}")
+        except:
+            continue
+
+
+def process_document(fp):
+    parser = PDFParser(fp)
+    doc = PDFDocument(parser)
+    if not doc.is_extractable:
+        raise PDFTextExtractionNotAllowed
+    mgr = PDFResourceManager()
+    device = PDFPageAggregator(mgr)
+    interpreter = PDFPageInterpreter(mgr, device)
+
+    contents = ""
+    for page in PDFPage.create_pages(doc):
+        interpreter.process_page(page)
+        layout = device.get_result()
+        for element in layout:
+            if isinstance(element, LTChar):
+                contents += element.get_text()
+    match = cases_pattern.search(contents)
+    if match:
+        return {k: int(v) for k, v in match.groupdict().items()}
+    return None
+
+
+if __name__ == "__main__":
+    download_files()
+    run_analisys()
