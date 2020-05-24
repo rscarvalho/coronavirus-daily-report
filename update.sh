6b848c20a7264a7e59bc122b60b98ed972f33154
Flavio Fernandes <flavio@flaviof.com>
2020-05-24 14:31:09 -0400

    Add Vagrant to automate data create/update
    This change uses vagrant to automate creation and update of
    data from a fedora VM, using python 3.8.
    
    In a system that has hypervisor (like libvirt or virtualBox):
    
    Do it once:
    - Install Vagrant (https://www.vagrantup.com/)
    - Install sshfs plugin: vagrant plugin install sshfs
    - Clone this repo
    
    Workflow cycle:
    - cd $repo
    - Run: vagrant up
    - Examine changes, commit, push
    - To update fetch new data: vagrant reload
    - To halt VM: vagrant halt
    - To blow VM away: vagrant destroy --force
    - Repeat until Covid-19 is no longer in your mind.
      I hope that to be soon!

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-pipenv run python ./script.py|tee stats.csv && \
-  pipenv run jupyter nbconvert --to notebook --inplace --execute Plots.ipynb
+#set -o xtrace
+set -o errexit
 
+pipenv run python ./script.py|tee stats.csv && {
+    hash dos2unix 2>/dev/null && {
+	dos2unix -k -n stats.csv{,.new} && mv -f stats.csv{.new,}
+    } ||:
+    pipenv run jupyter nbconvert --to notebook --inplace --execute Plots.ipynb
+}
